4b0dffcbb2da72ec227fb7ea827a49bc60d816db
Sheen <dan.80956@gmail.com>
2023-09-01 20:35:47 +0300

    +

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название')
+    description = models.TextField(verbose_name='описание', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField(upload_to='products/', verbose_name='картинки', null=True, blank=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за единицу')
+    date_of_creation = models.DateTimeField(verbose_name='дата создания')
+    date_of_change = models.DateTimeField(verbose_name='дата изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
